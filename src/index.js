b54f6c34f668c2538d9c361a2ff546fd00da20cc
Steve Richey <srichey@gowithfloat.com>
2019-02-27 17:56:09 -0600

    add quiz

--- src/index.js
+++ src/index.js
@@ -12,6 +12,32 @@ var app = new Vue({
       { name: "Woofer", type: "huge" },
       { name: "Pup", type: "tiny" }
     ],
+    quizQuestions: [
+      {
+        question: "Which of the following is a good Pokèmon?",
+        answers: [
+          "Pikachu",
+          "Garbador",
+          "Trubbish",
+          "Scorbunny"
+        ],
+        rightAnswer: "Scorbunny",
+        number: 1
+      },
+      {
+        question: "Why should I like myself?",
+        answers: [
+          "I try my best",
+          "Dogs are good",
+          "Sobbles is weird",
+          "Green dolphin"
+        ],
+        rightAnswer: "I try my best",
+        number: 2
+      }
+    ],
+    answeredQuestions: [],
+    quizScore: 0,
     firstName: "Steve",
     lastName: "Richey",
     pageName: "home",
@@ -31,6 +57,12 @@ var app = new Vue({
     },
     fileName: function () {
       return './images/' + this.currentColor + '_' + this.currentEmoji + '.png';
+    },
+    numberOfQuizQuestions: function () {
+      return this.quizQuestions.length;
+    },
+    quizPercent: function () {
+      return Math.round((this.quizScore / this.numberOfQuizQuestions) * 100);
     }
   },
   methods: {
@@ -42,6 +74,13 @@ var app = new Vue({
     },
     reverseOptions: function() {
       this.options.reverse();
+    },
+    submitAnswer: function(question, selectedAnswer) {
+      if (question.rightAnswer == selectedAnswer) {
+        this.quizScore += 1;
+      }
+
+      this.answeredQuestions.push(question.number);
     }
   }
 });
